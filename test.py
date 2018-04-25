33997fb86db99fb6dd86fb285a8367ad9c428f78
melo.gpm <melo.gpm@gmail.com>
2018-04-25 19:19:01 -0400

    update do readme

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "whatsapp, verificando o conteudo")
+        self.assertRegex(result.data.decode(), "eai blz consegui. DevOps")
 
 
 if __name__ == "__main__":
